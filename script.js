984d556d336888df67958a03f6749f0bbe68cd87
CubeKirito <cubekirito@cubesoft.asia>
2018-12-08 23:22:28 +0900

    Making frame...

---
+++ script.js
@@ -0,0 +1,50 @@
+var isWorking = false;
+
+$("document").ready(function() {
+    $("#enable_debug").click(function() {
+        function adddebug() {
+            var list = document.getElementsByClassName('question');
+            for (var i = 0; i < list.length; i++) {
+                document.getElementById(list[i].id).innerHTML += " (" + list[i].id + ")";
+            }
+        }
+        chrome.tabs.executeScript({
+            code: '(' + adddebug + ')();'
+        });
+    });
+    
+    chrome.tabs.executeScript({
+        code: 'document.domain'
+    }, function(result) {
+        if (result == "bdsmtest.org") {
+            $("#status").text('작동 중');
+            $("#status").css("color", "green");
+            
+            isWorking = true;
+            
+            // translate
+            function translate() {
+                document.querySelector(".toprow p").innerHTML = "다음 질문들에 얼마나 동의하시나요?";
+                
+                document.querySelector(".red.balance-text").innerHTML = "확실히<br />비동의";
+                document.querySelector(".yellow.balance-text").innerHTML = "중립 / 의견 없음";
+                document.querySelector(".green.balance-text").innerHTML = "확실히 동의";
+
+                var translations = {'q_15_text':'테스트'};
+                
+                var list = document.getElementsByClassName('question');
+                for (var i = 0; i < list.length; i++) {
+                    if (translations[list[i].id] != undefined) {
+                        document.getElementById(list[i].id).innerHTML = translations[list[i].id];
+                    }
+                }
+            }
+            chrome.tabs.executeScript({
+                code: '(' + translate + ')();'
+            });
+        } else {
+            $("#status").text('작동 중지 (테스트 페이지가 아닙니다)');
+            $("#status").css("color", "red");
+        }
+    });
+});
